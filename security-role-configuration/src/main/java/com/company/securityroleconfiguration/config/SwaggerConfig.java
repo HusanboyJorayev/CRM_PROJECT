078bed74468a4bf9e7cfd9771a15d2256f574a7f
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2023-10-22 23:55:26 +0500

    Update

--- security-role-configuration/src/main/java/com/company/securityroleconfiguration/config/SwaggerConfig.java
+++ security-role-configuration/src/main/java/com/company/securityroleconfiguration/config/SwaggerConfig.java
@@ -12,23 +12,87 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi groupedByCardAPI() {
-        return GroupedOpenApi.builder()
-                .group("Card")
-                .pathsToMatch("/card/**")
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi groupedByUsersAPI() {
+    public GroupedOpenApi publicUserApi(){
         return GroupedOpenApi.builder()
                 .group("User")
-                .pathsToMatch("/users/**")
+                .pathsToMatch("/user/**")
                 .build();
     }
 
     @Bean
-    public GroupedOpenApi groupedByTeacherAPI() {
+    public GroupedOpenApi publicCourseFeesApi(){
+        return GroupedOpenApi.builder()
+                .group("CourseFees")
+                .pathsToMatch("/coFeeCon/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicEmployeeApi(){
+        return GroupedOpenApi.builder()
+                .group("Employee")
+                .pathsToMatch("/emp/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicFinanceApi(){
+        return GroupedOpenApi.builder()
+                .group("Finance")
+                .pathsToMatch("/finance/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicGroupsApi(){
+        return GroupedOpenApi.builder()
+                .group("Groups")
+                .pathsToMatch("/groups/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicRoleApi(){
+        return GroupedOpenApi.builder()
+                .group("Role")
+                .pathsToMatch("/role/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicRoomApi(){
+        return GroupedOpenApi.builder()
+                .group("Room")
+                .pathsToMatch("/room/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicSalaryApi(){
+        return GroupedOpenApi.builder()
+                .group("Salary")
+                .pathsToMatch("/salary/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicStudentApi(){
+        return GroupedOpenApi.builder()
+                .group("Student")
+                .pathsToMatch("/student/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicSubjectApi(){
+        return GroupedOpenApi.builder()
+                .group("Subject")
+                .pathsToMatch("/subject/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicTeacherApi(){
         return GroupedOpenApi.builder()
                 .group("Teacher")
                 .pathsToMatch("/teacher/**")
